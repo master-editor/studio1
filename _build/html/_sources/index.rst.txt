600c44d40ccee72dc13721956d1e04dd3b769264
1 <airdata@abv.bg>
2017-06-03 03:12:00 +0300

    add

---
+++ _build/html/_sources/index.rst.txt
@@ -0,0 +1,39 @@
+    сТартира след разпада на екип "Студио1|БНР"
+
+..  image:: img/s1.png
+   :height: 351px
+   :width: 300px
+   :scale: 50%
+   :alt: ENS logo
+   :align: right
+
+.. toctree::
+  :maxdepth: 1
+  :caption: ОБОРУДВАНЕ
+
+  equipment/computer-systems
+  equipment/mixers-tracks
+  equipment/sound-systems
+
+.. toctree::
+  :maxdepth: 1
+  :caption: ИНСТРУМЕНТИ
+
+  instruments/acoustic
+  instruments/analog
+  instruments/midi
+
+
+.. toctree::
+  :maxdepth: 1
+  :caption: АПАРАТНА
+
+  room/env
+  room/workstation
+  room/racks
+
+
+  ЗВУКОЗАПИС
+  ИНЖИНЕРИ
+  ЦЕНИ
+  РЪКОВОДСТВА
---
+++ conf.py
@@ -0,0 +1,45 @@
+import os
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary'
+    ]
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'STUDIO 1'
+version = u'SOFIA'
+release = 'SOFIA'
+copyright = 'CBMAK 2017'
+author = 'Rumen Lishkov'
+
+#templates_path = ['_templates']
+exclude_patterns = ['_build', '.idea', '.project', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If truetodo and todoList produce output, else they produce nothing.
+todo_include_todos = True
+
+# -- Options for HTMLHelp output ------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['static']
+
+#html_theme_path = ["_themes", ]
+# Theme style override
+
+# Output file base name for HTML help builder.
+#htmlhelp_basename = 'STUDIO 1'
+
+if os.environ.get('READTHEDOCS') == 'True':
+    style_file = '_static/theme.css'
+else:
+    html_style = 'theme.css'
+    style_file = '_static/css/theme.css'
+
+html_context = {
+    'css_files': [style_file]
+}
